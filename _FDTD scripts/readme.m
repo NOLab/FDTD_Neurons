a2dba2ed8bb9967328b0f221196867c619c503e1
NOLab <49475144+NOLab@users.noreply.github.com>
2025-02-04 14:31:36 +0100

    Update readme.m

--- _FDTD scripts/readme.m
+++ _FDTD scripts/readme.m
@@ -1,1 +1,10 @@
-sample FDTD simulation for a given axon diameter (1500 nm) au myelin thickness (200nm)
+sample FDTD simulation for a given axon diameter (1750 nm) au myelin thickness (200nm)
+
+Material database: need to create or import 3 materials:
+WATER: dielectric, n=1.33
+EXTRACELL139: extracellular medium (dieletric, n=1.39)
+AXON137: interirior of the axon, (dielectric n=1.37)
+MYELIN143: myelin, (dielectric n=1.43)
+EXTRACELL139_NL:chi3, 1.9 e-5
+Axon137_NL:chi3 1.7e-5
+MYELIN143_NL: chi3, 2.5e-5
